7e532083d37183ef322b5e844525884f21d645fb
Manuel Thalmann <m@nuth.ch>
2018-11-09 14:22:10 +0100

    Initialize a new module

---
+++ src/tests/main.test.ts
@@ -0,0 +1,13 @@
+import * as assert from "assert";
+
+suite(
+    "TempFileSystem",
+    () =>
+    {
+        test(
+            "Example...",
+            () =>
+            {
+                assert.strictEqual(1, 1);
+            });
+    });
