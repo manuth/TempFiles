0576ceeaf98febaf40ad51fe2ec1deb0f31b6729
Manuel Thalmann <m@nuth.ch>
2018-11-09 16:25:13 +0100

    Add basic functionalities.

---
+++ src/TempFile.ts
@@ -0,0 +1,24 @@
+import { fileSync, Options } from "tmp";
+import { TempFileSystem } from "./TempFileSystem";
+
+/**
+ * Represents a temporary file.
+ */
+export class TempFile extends TempFileSystem
+{
+    /**
+     * Initializes a new instance of the `TempFile` class.
+     *
+     * @param options
+     * The options for the initialization.
+     */
+    public constructor(options: Options)
+    {
+        super(options);
+    }
+
+    protected Initialize(options: Options): void
+    {
+        this.TempFileSystemEntry = fileSync(options);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("Hello World");
+export { Options, SynchrounousResult } from "tmp";
+export { TempFileSystem } from "./TempFileSystem";
+export { TempFile } from "./TempFile";
+export { TempDirectory } from "./TempDirectory";
