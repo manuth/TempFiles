f7e16a135f18a266040e18fea2a9d455a43838fb
Manuel Thalmann <m@nuth.ch>
2020-09-08 23:11:55 +0200

    Add tests for keeping files

--- src/tests/TempFileSystem.test.ts
+++ src/tests/TempFileSystem.test.ts
@@ -2,7 +2,7 @@ import Assert = require("assert");
 import { ChildProcess, fork } from "child_process";
 import { tmpdir } from "os";
 import { isAbsolute, relative } from "path";
-import { pathExists } from "fs-extra";
+import { pathExists, remove } from "fs-extra";
 import RandExp = require("randexp");
 import { ITempNameOptions } from "../ITempNameOptions";
 import { TempFile } from "../TempFile";
@@ -24,18 +24,29 @@ export function TempFileSystemTests(): void
                 {
                     let files: ITestFiles;
 
+                    /**
+                     * Spawns the test-script.
+                     *
+                     * @param keep
+                     * A value indicating whether the generated test-files should be kept.
+                     */
+                    async function SpawnTestScript(keep: boolean): Promise<void>
+                    {
+                        let child: ChildProcess;
+                        child = fork(require.resolve("./createTempFile"), keep ? ["keep"] : []);
+                        child.on("message", (message: ITestFiles) => files = message);
+
+                        return new Promise(
+                            (resolve) =>
+                            {
+                                child.on("exit", () => resolve());
+                            });
+                    }
+
                     suiteSetup(
                         async function()
                         {
-                            let child: ChildProcess;
-                            child = fork(require.resolve("./createTempFile"));
-                            child.on("message", (message: ITestFiles) => files = message);
-
-                            await new Promise(
-                                (resolve) =>
-                                {
-                                    child.on("exit", () => resolve());
-                                });
+                            return SpawnTestScript(false);
                         });
 
                     test(
@@ -50,6 +61,20 @@ export function TempFileSystemTests(): void
                         async () =>
                         {
                             Assert.ok(!await pathExists(files.dir));
+                        });
+
+                    test(
+                        "Checking whether temporary files can be kept even after `process.exit` occurs…",
+                        async function()
+                        {
+                            this.timeout(5 * 1000);
+                            this.slow(3 * 1000);
+
+                            await SpawnTestScript(true);
+                            Assert.ok(await pathExists(files.file));
+                            Assert.ok(await pathExists(files.dir));
+                            await remove(files.file);
+                            await remove(files.dir);
                         });
                 });
         });
